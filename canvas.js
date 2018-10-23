2da7b51685929c29fc3cd06bf77d00520ffa61ab
Henry Withers <hellohenry@gmail.com>
2018-10-23 14:57:17 +0100

    some changes at work

--- canvas.js
+++ canvas.js
@@ -1,15 +1,18 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-console.log(ctx.globalCompositeOperation);
-// ctx.globalCompositeOperation = "difference";
+console.log("linewidth", ctx.lineWidth);
 
-ctx.lineWidth = 8;
+width = canvas.width = window.innerWidth;
+height = canvas.height = width * 0.4;
 
-const width = canvas.width;
-const height = canvas.height;
+const velocity = 0.6 || width / 250;
 
 const letterHeight = height * 0.53;
+
+ctx.lineWidth = 2;
+ctx.shadowBlur = 2;
+ctx.shadowColor = "white";
 
 let xEnd = 0;
 let yEnd = 0;
@@ -71,16 +74,18 @@ const aEndCoords = [aStartCoords[0], aStartCoords[1] + letterHeight];
 
 const aStartCoords2 = [aStartCoords[0] + 2 * aXEndPoint, aStartCoords[1]];
 const aEndCoords2 = [aStartCoords2[0], aStartCoords2[1] + letterHeight];
-
-const aStartCoordsFinal = [tStartPoint[0], height * 0.225 + letterHeight / 2];
+// was letterHeight / 2
+const aStartCoordsFinal = [tStartPoint[0], height * 0.225 + letterHeight / 2.5];
 
 aXEndPointFinal = width * 0.65;
 
 // vars for lightShow
 
-const lightShowStartCoords = [0, height * 0.5];
+const lightShowStartCoords = [width * 0.1, height * 0.1];
 let xEndLightShow = 0;
 let yEndLightShow = 0;
+
+const lightShowVelocity = 3 || width / 300;
 
 fillBackground();
 
@@ -97,6 +102,7 @@ let imageData = ctx.getImageData(
 
 animateA();
 var finalImage;
+var lightShowImage;
 
 function drawLineYDiagonal(initialCoords, endCoords, variable, colour) {
   ctx.beginPath();
@@ -106,57 +112,131 @@ function drawLineYDiagonal(initialCoords, endCoords, variable, colour) {
   ctx.stroke();
 }
 
+let xToggle = 0;
+let yToggle = 0;
+
 function lightShow() {
   if (100 < 101) {
-    requestAnimationFrame(lightShow);
+    // requestAnimationFrame(lightShow);
+    console.log("would be animating");
   } else {
     console.log("finished");
   }
-  ctx.globalCompositeOperation = "destination-atop";
-  // ctx.putImageData(finalImage, 0, 0);
-  ctx.moveTo(
-    lightShowStartCoords[0] + xEndLightShow,
-    lightShowStartCoords[1] + yEndLightShow
-  );
-  ctx.lineTo(lightShowStartCoords[0] + 10, lightShowStartCoords[1]);
-  ctx.strokeStyle = "yellow";
-  ctx.stroke();
-  if (xEndLightShow < width) {
-    xEndLightShow++;
-  } else {
-    xEndLightShow--;
-  }
+
+  ctx.putImageData(finalImage, 0, 0);
+  // ctx.beginPath();
+  // ctx.arc(
+  //   lightShowStartCoords[0] + xEndLightShow,
+  //   lightShowStartCoords[1] + yEndLightShow,
+  //   Math.abs(xEndLightShow) / 3,
+  //   0,
+  //   2 * Math.PI
+  // );
+  // ctx.fillStyle = "pink";
+  // ctx.fill();
+
+  // ctx.strokeStyle = "pink";
+
+  // if (xEndLightShow >= width * 0.8 || xEndLightShow < 0) {
+  //   xToggle = !xToggle ? 1 : 0;
+  // }
+
+  // if (yEndLightShow >= height * 0.7 || yEndLightShow < 0) {
+  //   yToggle = !yToggle ? 1 : 0;
+  // }
+
+  // xEndLightShow = !xToggle
+  //   ? xEndLightShow + lightShowVelocity
+  //   : xEndLightShow - lightShowVelocity;
+  // yEndLightShow = !yToggle
+  //   ? yEndLightShow + 0.2 * lightShowVelocity
+  //   : yEndLightShow - 0.2 * lightShowVelocity;
 }
 
 function animateAFinal() {
   if (aXEndFinal < aXEndPointFinal) {
     requestAnimationFrame(animateAFinal);
+
+    ctx.putImageData(finalImage, 0, 0);
+    finalImage = ctx.getImageData(0, 0, width, height);
+    ctx.globalCompositeOperation = "source-over";
+
+    // horiz A lines
+
+    ctx.beginPath();
+    ctx.moveTo(aStartCoords[0], aStartCoords[1] + letterHeight);
+    ctx.lineTo(
+      aStartCoords[0] + letterHeight / 6,
+      aStartCoords[1] + letterHeight
+    );
+    //diag A Line
+    ctx.lineTo(
+      aStartCoords[0] + aXEndPoint,
+      aStartCoords[1] + 0.5 * letterHeight
+    );
+    ctx.strokeStyle = otherAColour;
+    ctx.stroke();
+
+    // horiz A line -
+
+    ctx.beginPath();
+    ctx.moveTo(
+      aStartCoords[0] + 2 * aXEndPoint,
+      aStartCoords[1] + letterHeight
+    );
+    ctx.lineTo(
+      aStartCoords[0] + 2 * aXEndPoint - letterHeight / 6,
+      aStartCoords[1] + letterHeight
+    );
+    //diag A line -
+    ctx.lineTo(
+      aStartCoords[0] + aXEndPoint,
+      aStartCoords[1] + 0.5 * letterHeight
+    );
+    ctx.strokeStyle = otherAColour;
+    ctx.strokeStyle = otherAColour;
+    ctx.stroke();
+
+    // A cross line
+    // ctx.globalCompositeOperation = "difference";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(aStartCoordsFinal[0] + aXEndFinal / 1.35, aStartCoordsFinal[1]);
+    ctx.lineTo(aStartCoordsFinal[0] + aXEndFinal, aStartCoordsFinal[1]);
+    ctx.lineTo(
+      aStartCoordsFinal[0] + aXEndFinal,
+      aStartCoordsFinal[1] + letterHeight / 7
+    );
+    ctx.lineTo(
+      aStartCoordsFinal[0] + aXEndFinal / 1.35,
+      aStartCoordsFinal[1] + letterHeight / 7
+    );
+    ctx.lineTo(aStartCoordsFinal[0] + aXEndFinal / 1.35, aStartCoordsFinal[1]);
+
+    ctx.strokeStyle = "yellow";
+    ctx.stroke();
+
+    aXEndFinal = aXEndFinal + 7;
   } else {
     console.log("done AFinal");
+    finalImage = ctx.getImageData(0, 0, width, height);
+    ctx.globalCompositeOperation = "destination-in";
     lightShow();
   }
-
-  ctx.putImageData(finalImage, 0, 0);
-  finalImage = ctx.getImageData(0, 0, width, height);
-  ctx.beginPath();
-  ctx.moveTo(aStartCoordsFinal[0] + aXEndFinal / 1.35, aStartCoordsFinal[1]);
-  ctx.lineTo(aStartCoordsFinal[0] + aXEndFinal, aStartCoordsFinal[1]);
-  ctx.strokeStyle = "yellow";
-  ctx.stroke();
-  aXEndFinal = aXEndFinal + 7;
 }
+
+var otherAColour = "";
 
 function animateA() {
   if (aXEnd < aXEndPoint) {
     requestAnimationFrame(animateA);
   } else {
     // what's going on here
-    // drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
-    // animateAFinal();
-    lightShow();
+    drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
   }
   var aColour = `rgb(${150}, ${10 + ((aXEnd * 1) % 255)}, ${10 +
     ((aXEnd * 2) % 255)})`;
+  otherAColour = aColour;
   ctx.putImageData(imageData, aStartCoords[0] - 5, aStartCoords[1] - 5);
 
   drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
@@ -190,7 +270,7 @@ function animateD() {
   ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd * 1) % 255)}, ${10 +
     ((dYEnd * 2) % 255)})`;
   ctx.stroke();
-  dYEnd++;
+  dYEnd = dYEnd + velocity;
   if (dXEnd < Math.PI / 2) {
     dXEnd = dXEnd + 0.03;
   }
@@ -199,35 +279,35 @@ function animateD() {
 function animateDStep2() {
   if (dYEnd2 < dYEndPoint2) {
     window.requestAnimationFrame(animateDStep2);
+    drawLineYForwards(
+      [dStartPoint[0], dStartPoint[1] + width * 0.04],
+      dXEndPoint,
+      dYEnd2,
+      `rgb(${150}, ${10 + ((dYEnd2 * 2) % 255)}, ${50 + ((dYEnd2 * 3) % 255)})`
+    );
+    ctx.beginPath();
+    ctx.ellipse(
+      dStartPoint[0] + dXEndPoint,
+      dStartPoint[1] + dYEndPoint2 / 2 + width * 0.04,
+      dYEndPoint2 / 2 + width * 0.0,
+      dYEndPoint2 / 2,
+      0,
+      -Math.PI / 2,
+      dXEnd2
+    );
+    ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd2 * 1) % 255)}, ${10 +
+      ((dYEnd2 * 3) % 255)})`;
+    ctx.stroke();
+    dYEnd2 = dYEnd2 + velocity;
+    if (dXEnd2 < Math.PI / 2) {
+      dXEnd2 = dXEnd2 + 0.05;
+    }
   } else {
     console.log("done D");
 
     finalImage = ctx.getImageData(0, 0, width, height);
 
     animateAFinal();
-  }
-  drawLineYForwards(
-    [dStartPoint[0], dStartPoint[1] + width * 0.04],
-    dXEndPoint,
-    dYEnd2,
-    `rgb(${150}, ${10 + ((dYEnd2 * 2) % 255)}, ${50 + ((dYEnd2 * 3) % 255)})`
-  );
-  ctx.beginPath();
-  ctx.ellipse(
-    dStartPoint[0] + dXEndPoint,
-    dStartPoint[1] + dYEndPoint2 / 2 + width * 0.04,
-    dYEndPoint2 / 2 + width * 0.0,
-    dYEndPoint2 / 2,
-    0,
-    -Math.PI / 2,
-    dXEnd2
-  );
-  ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd2 * 1) % 255)}, ${10 +
-    ((dYEnd2 * 3) % 255)})`;
-  ctx.stroke();
-  dYEnd2++;
-  if (dXEnd2 < Math.PI / 2) {
-    dXEnd2 = dXEnd2 + 0.05;
   }
 }
 
@@ -237,13 +317,15 @@ function animateT() {
   } else {
     animateTStep2();
   }
+
   tColour = `rgb(${0}, ${100 + ((xEnd * 1) % 255)}, 100)`;
+  ctx.shadowColor = tColour;
   drawLineXForwards(tStartPoint, 0, xEnd, tColour);
   drawLineYForwards(tStartPoint, 0, yEnd, tColour);
-  xEnd++;
-  xEnd++;
+  xEnd = xEnd + velocity;
+  xEnd = xEnd + velocity;
   if (yEnd < tYEndPoint) {
-    yEnd++;
+    yEnd = yEnd + velocity;
   }
 }
 
@@ -255,11 +337,11 @@ function animateTStep2() {
   }
   drawLineXForwards(tStartPoint, tYEndPoint, xEnd2, tColour);
   drawLineYForwards(tStartPoint, tXEndPoint, yEnd2, tColour);
-  xEnd2++;
-  xEnd2++;
+  xEnd2 = xEnd2 + velocity;
+  xEnd2 = xEnd2 + velocity;
   if (yEnd2 < tYEndPoint2) {
-    yEnd2++;
-    yEnd2++;
+    yEnd2 = yEnd2 + velocity;
+    yEnd2 = yEnd2 + velocity;
   }
 }
 
@@ -282,10 +364,10 @@ function animateTStep3() {
     -xEnd3,
     tColour
   );
-  yEnd3++;
-  yEnd3++;
+  yEnd3 = yEnd3 + velocity;
+  yEnd3 = yEnd3 + velocity;
   if (xEnd3 < tXEndPoint3) {
-    xEnd3++;
+    xEnd3 = xEnd3 + velocity;
   }
 }
 
@@ -307,10 +389,10 @@ function animateTStep4() {
     yEnd4,
     tColour
   );
-  yEnd4++;
-  yEnd4++;
+  yEnd4 = yEnd4 + velocity;
+  yEnd4 = yEnd4 + velocity;
   if (xEnd4 < tXEndPoint4) {
-    xEnd4++;
+    xEnd4 = xEnd4 + velocity;
   }
 }
 
