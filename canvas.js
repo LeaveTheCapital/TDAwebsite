09cffe84e150ae0a5f12fb19f7b7d36bdbdd3cbc
Henry Withers <hellohenry@gmail.com>
2021-01-01 14:38:07 +0000

    refactor to make animation functions take state and be more pure

--- canvas.js
+++ canvas.js
@@ -1,97 +1,108 @@
 let canvas = document.getElementById("canvas");
+canvas.style.border = "1px solid yellow";
 let ctx = canvas.getContext("2d");
 
 // ctx.globalCompositeOperation = "difference";
 ctx.lineCap = "square";
-ctx.lineWidth = 5;
+ctx.lineWidth = 2;
 
+//consts for size
+
+const numberOfLetters = 3;
 const width = canvas.width;
+const letterWidth = width / (numberOfLetters + 2);
+const paddingAroundLetters =
+  (width - numberOfLetters * letterWidth) / (numberOfLetters + 1);
+
+canvas.height = letterWidth + 2 * paddingAroundLetters;
+
 const height = canvas.height;
 
-const letterHeight = height * 0.54;
+const letterHeight = letterWidth;
 
-let xEnd = 0;
-let yEnd = 0;
-
-let xEnd2 = 0;
-let yEnd2 = 0;
-
-let xEnd3 = 0;
-let yEnd3 = 0;
-
-let xEnd4 = 0;
-let yEnd4 = 0;
-
-const cdolour = "cornflowerblue";
-let tColour = `rgb(${0}, ${100 + ((xEnd * 1) % 255)}, ${
-  100 + ((xEnd * 3) % 255)
-})`;
-
-// vars for letter T
-const tStartPoint = [width * 0.12, height * 0.225];
-
-const tXEndPoint = width * 0.2;
 const tYEndPoint = height * 0.13;
-
 const tXEndPoint2 = width * 0.07;
-const tYEndPoint2 = tYEndPoint;
+const tYEndPoint3 = letterHeight - tYEndPoint;
 
-const tXEndPoint3 = tXEndPoint2;
-const tYEndPoint3 = height * 0.4;
+const tInitialState = {
+  xEnd: 0,
+  yEnd: 0,
+  tStartPoint: [paddingAroundLetters, paddingAroundLetters],
+  tYEndPoint: height * 0.13,
+  tXEndPoint2: width * 0.07,
+  tYEndPoint2: height * 0.13,
+  tXEndPoint3: width * 0.07,
+  tYEndPoint3: tYEndPoint3,
+  tXEndPoint4: letterWidth - 2 * tXEndPoint2,
+  tYEndPoint4: letterHeight - tYEndPoint,
+  tColour: `rgb(${0}, ${100 + ((0 * 1) % 255)}, ${100 + ((0 * 3) % 255)})`,
+};
 
-const tXEndPoint4 = tXEndPoint - 2 * tXEndPoint2;
-const tYEndPoint4 = tYEndPoint3;
+const dInitialState = {
+  dXEnd: -Math.PI / 2,
+  dYEnd: 0,
 
-// vars for letter D
-let dXEnd = -Math.PI / 2;
-let dYEnd = 0;
+  dStartPoint: [letterWidth + 2 * paddingAroundLetters, paddingAroundLetters],
+  dColour: `rgb(${150}, ${10 + ((0 * 3) % 255)}, ${10 + ((0 * 3) % 255)})`,
 
-const dStartPoint = [width * 0.44, height * 0.225];
-let dColour = `rgb(${150}, ${10 + ((dYEnd * 3) % 255)}, ${
-  10 + ((dYEnd * 3) % 255)
-})`;
+  dYEndPoint: letterHeight,
 
-const dYEndPoint = height * 0.53;
+  dXEnd2: -Math.PI / 2,
+  dYEnd2: 0,
 
-let dXEnd2 = -Math.PI / 2;
-let dYEnd2 = 0;
+  dXEndPoint: width * 0.0275,
+  dYEndPoint2: tYEndPoint + tYEndPoint3 - width * 0.08,
+};
 
-const dXEndPoint = width * 0.0275;
-const dYEndPoint2 = tYEndPoint + tYEndPoint3 - width * 0.08;
+// const aXEndPoint = width * 0.07; // old value for narrower A
 
-// vars for letter A
-let aXEnd = 0;
-let aXEnd2 = 0;
-let aXEndFinal = 0;
+const aStartCoords = [
+  3 * paddingAroundLetters + 2 * letterWidth,
+  paddingAroundLetters,
+];
 
-const aXEndPoint = width * 0.07;
+const aStartCoords2 = [aStartCoords[0] + letterWidth, paddingAroundLetters];
 
-const aStartCoords = [width * 0.69, height * 0.225];
-const aEndCoords = [aStartCoords[0], aStartCoords[1] + letterHeight];
+const aInitialState = {
+  aXEnd: 0,
+  aXEnd2: 0,
+  narrowAXEndPoint: width * 0.07,
 
-const aStartCoords2 = [aStartCoords[0] + 2 * aXEndPoint, aStartCoords[1]];
-const aEndCoords2 = [aStartCoords2[0], aStartCoords2[1] + letterHeight];
+  aStartCoords: [
+    3 * paddingAroundLetters + 2 * letterWidth,
+    paddingAroundLetters,
+  ],
+  aEndCoords: [aStartCoords[0], paddingAroundLetters + letterHeight],
+  aStartCoords2: [aStartCoords[0] + letterWidth, paddingAroundLetters],
+  aEndCoords2: [aStartCoords2[0], paddingAroundLetters + letterHeight],
+};
 
-const aStartCoordsFinal = [tStartPoint[0], height * 0.225 + letterHeight / 2];
-
-aXEndPointFinal = width * 0.74;
+const aFinalInitialState = {
+  aStartCoordsFinal: [
+    paddingAroundLetters,
+    paddingAroundLetters + letterHeight / 2,
+  ],
+  aXEndFinal: 0,
+  // aXEndPointFinal: width * 0.74, // old value from narrower A
+  aXEndPointFinal: width - letterWidth,
+};
 
 // vars for lightShow
 const lightShowStartCoords = [0, height * 0.5];
 let xEndLightShow = 0;
 let yEndLightShow = 0;
 
-fillBackground();
+// fillBackground();
 
 let iFlash = 0;
 let jFlash = 0;
 let iColour = 0;
 
-flashBackground();
+// flashBorder();
 
-animateT();
+window.requestAnimationFrame(() => animateT(0, tInitialState));
 
-animateD();
+window.requestAnimationFrame(() => animateD(0, dInitialState));
 
 let imageData = ctx.getImageData(
   aStartCoords[0] - 5,
@@ -100,131 +111,70 @@ let imageData = ctx.getImageData(
   height - aStartCoords[1]
 );
 
-animateA();
-var finalImage;
+window.requestAnimationFrame(() => animateA(0, aInitialState));
 
-function drawLineYDiagonal(initialCoords, endCoords, variable, colour) {
-  ctx.beginPath();
-  ctx.moveTo(initialCoords[0] + variable, initialCoords[1]);
-  ctx.lineTo(endCoords[0], endCoords[1]);
-  ctx.strokeStyle = colour;
-  ctx.stroke();
-}
+drawGrid();
 
-function animateAFinal() {
-  if (aXEndFinal < aXEndPointFinal) {
-    requestAnimationFrame(animateAFinal);
-  } else {
-    console.log("done AFinal");
-    // initiate final flourish
-    // flashBackground();
-    // lightShow();
-  }
+// temp to show grid lines
+function drawGrid() {
+  if (false) {
+    const colours = ["red", "yellow", "green", "white", "orange"];
+    for (let i = 0; i <= numberOfLetters; i++) {
+      // draw lines in between
+      drawLineXForwards({
+        initialCoords: [
+          letterWidth * i + paddingAroundLetters * i,
+          paddingAroundLetters,
+        ],
+        yPositionOffset: 0,
+        length: paddingAroundLetters,
+        colour: colours[2],
+      });
 
-  ctx.putImageData(finalImage, 0, 0);
-  finalImage = ctx.getImageData(0, 0, width, height);
-  ctx.beginPath();
-  ctx.moveTo(aStartCoordsFinal[0] + aXEndFinal / 1.35, aStartCoordsFinal[1]);
-  ctx.lineTo(aStartCoordsFinal[0] + aXEndFinal, aStartCoordsFinal[1]);
-  ctx.strokeStyle = "yellow";
-  ctx.stroke();
-  aXEndFinal = aXEndFinal + 7;
-}
+      drawLineXForwards({
+        initialCoords: [
+          letterWidth * i + paddingAroundLetters * (i + 1),
+          paddingAroundLetters,
+        ],
+        yPositionOffset: 0,
+        length: letterWidth,
+        colour: colours[3],
+      });
 
-function animateA() {
-  if (aXEnd < aXEndPoint) {
-    requestAnimationFrame(animateA);
-  } else {
-    drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
-  }
-  var aColour = `rgb(${150}, ${10 + ((aXEnd * 1) % 255)}, ${
-    10 + ((aXEnd * 2) % 255)
-  })`;
-  ctx.putImageData(imageData, aStartCoords[0] - 5, aStartCoords[1] - 5);
+      drawLineXForwards({
+        initialCoords: [
+          letterWidth * i + paddingAroundLetters * i,
+          height - paddingAroundLetters,
+        ],
+        yPositionOffset: 0,
+        length: paddingAroundLetters,
+        colour: colours[2],
+      });
 
-  drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
-  drawLineYDiagonal(aStartCoords2, aEndCoords2, -aXEnd2, aColour);
-  aXEnd = aXEnd + 0.35;
-  aXEnd2 = aXEnd2 + 0.35;
-}
-
-function animateD() {
-  if (dYEnd < dYEndPoint) {
-    window.requestAnimationFrame(animateD);
-  } else {
-    animateDStep2();
-  }
-  drawLineYForwards({
-    initialCoords: dStartPoint,
-    endPoint: 0,
-    length: dYEnd,
-    colour: `rgb(${150}, ${10 + ((dYEnd * 1) % 255)}, ${
-      10 + ((dYEnd * 2) % 255)
-    })`,
-  });
-  ctx.beginPath();
-  ctx.ellipse(
-    dStartPoint[0],
-    dStartPoint[1] + dYEndPoint / 2,
-    dYEndPoint / 2 + width * 0.02,
-    dYEndPoint / 2,
-    0,
-    -Math.PI / 2,
-    dXEnd
-  );
-  ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd * 1) % 255)}, ${
-    10 + ((dYEnd * 2) % 255)
-  })`;
-  ctx.stroke();
-  dYEnd++;
-  if (dXEnd < Math.PI / 2) {
-    dXEnd = dXEnd + 0.03;
+      drawLineXForwards({
+        initialCoords: [
+          letterWidth * i + paddingAroundLetters * (i + 1),
+          height - paddingAroundLetters,
+        ],
+        yPositionOffset: 0,
+        length: letterWidth,
+        colour: colours[3],
+      });
+    }
   }
 }
 
-function animateDStep2() {
-  if (dYEnd2 < dYEndPoint2) {
-    window.requestAnimationFrame(animateDStep2);
+let finalImage;
+
+function animateT(ms, tState) {
+  let state = { ...tState };
+  let { xEnd, tStartPoint, tColour, yEnd, tYEndPoint } = state;
+  if (xEnd < letterWidth) {
+    window.requestAnimationFrame((ts) => animateT(ts, state));
   } else {
-    console.log("done D");
-
-    finalImage = ctx.getImageData(0, 0, width, height);
-
-    animateAFinal();
-  }
-  drawLineYForwards({
-    initialCoords: [dStartPoint[0], dStartPoint[1] + width * 0.04],
-    endPoint: dXEndPoint,
-    length: dYEnd2,
-    colour: `rgb(${150}, ${10 + ((dYEnd2 * 2) % 255)}, ${
-      50 + ((dYEnd2 * 3) % 255)
-    })`,
-  });
-  ctx.beginPath();
-  ctx.ellipse(
-    dStartPoint[0] + dXEndPoint,
-    dStartPoint[1] + dYEndPoint2 / 2 + width * 0.04,
-    dYEndPoint2 / 2 + width * 0.0,
-    dYEndPoint2 / 2,
-    0,
-    -Math.PI / 2,
-    dXEnd2
-  );
-  ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd2 * 1) % 255)}, ${
-    10 + ((dYEnd2 * 3) % 255)
-  })`;
-  ctx.stroke();
-  dYEnd2++;
-  if (dXEnd2 < Math.PI / 2) {
-    dXEnd2 = dXEnd2 + 0.05;
-  }
-}
-
-function animateT() {
-  if (xEnd < tXEndPoint) {
-    window.requestAnimationFrame(animateT);
-  } else {
-    animateTStep2();
+    window.requestAnimationFrame(() =>
+      animateTStep2(ms, { ...state, xEnd: 0, yEnd: 0 })
+    );
   }
   tColour = `rgb(${0}, ${100 + ((xEnd * 1) % 255)}, 100)`;
   drawLineXForwards({
@@ -241,86 +191,258 @@ function animateT() {
   });
   xEnd++;
   xEnd++;
+
   if (yEnd < tYEndPoint) {
     yEnd++;
   }
+  state = { ...state, tColour, xEnd, yEnd };
 }
 
-function animateTStep2() {
-  if (xEnd2 < tXEndPoint2) {
-    window.requestAnimationFrame(animateTStep2);
+function animateTStep2(ms, tState) {
+  let state = { ...tState };
+  let {
+    xEnd,
+    tXEndPoint2,
+    tStartPoint,
+    tColour,
+    yEnd,
+    tYEndPoint,
+    tYEndPoint2,
+  } = state;
+
+  if (xEnd < tXEndPoint2) {
+    window.requestAnimationFrame((ts) => animateTStep2(ts, state));
   } else {
-    animateTStep3();
+    window.requestAnimationFrame(() =>
+      animateTStep3(ms, { ...state, xEnd: 0, yEnd: 0 })
+    );
   }
   drawLineXForwards({
     initialCoords: tStartPoint,
     yPositionOffset: tYEndPoint,
-    length: xEnd2,
+    length: xEnd,
     colour: tColour,
   });
   drawLineYForwards({
     initialCoords: tStartPoint,
-    endPoint: tXEndPoint,
-    length: yEnd2,
+    endPoint: letterWidth,
+    length: yEnd,
     colour: tColour,
   });
-  xEnd2++;
-  xEnd2++;
-  if (yEnd2 < tYEndPoint2) {
-    yEnd2++;
-    yEnd2++;
+  xEnd++;
+  xEnd++;
+  if (yEnd < tYEndPoint2) {
+    yEnd++;
+    yEnd++;
   }
+  state = { ...state, xEnd, yEnd };
 }
 
-function animateTStep3() {
-  if (yEnd3 < tYEndPoint3) {
-    window.requestAnimationFrame(animateTStep3);
+function animateTStep3(ms, tState) {
+  let state = { ...tState };
+  let { tStartPoint, tColour, xEnd, yEnd, tYEndPoint2, tXEndPoint3 } = state;
+  if (yEnd < tYEndPoint3) {
+    window.requestAnimationFrame(() => animateTStep3(ms, state));
   } else {
-    animateTStep4();
+    window.requestAnimationFrame(() =>
+      animateTStep4(ms, { ...state, xEnd: 0, yEnd: 0 })
+    );
   }
 
   drawLineYForwards({
     initialCoords: [tStartPoint[0], tStartPoint[1] + tYEndPoint2],
     endPoint: tXEndPoint2,
-    length: yEnd3,
+    length: yEnd,
     colour: tColour,
   });
   drawLineXForwards({
-    initialCoords: [tStartPoint[0] + tXEndPoint, tStartPoint[1]],
+    initialCoords: [tStartPoint[0] + letterWidth, tStartPoint[1]],
     yPositionOffset: tYEndPoint2,
-    length: -xEnd3,
+    length: -xEnd,
     colour: tColour,
   });
-  yEnd3++;
-  yEnd3++;
-  if (xEnd3 < tXEndPoint3) {
-    xEnd3++;
+  yEnd++;
+  yEnd++;
+  if (xEnd < tXEndPoint3) {
+    xEnd++;
   }
+  state = { ...state, xEnd, yEnd };
 }
 
-function animateTStep4() {
-  if (yEnd4 < tYEndPoint4) {
-    window.requestAnimationFrame(animateTStep4);
+function animateTStep4(ms, tState) {
+  let state = { ...tState };
+  let {
+    xEnd,
+    tStartPoint,
+    tXEndPoint2,
+    tColour,
+    yEnd,
+    tYEndPoint,
+    tYEndPoint2,
+    tXEndPoint4,
+    tYEndPoint4,
+  } = state;
+  if (yEnd < tYEndPoint4) {
+    window.requestAnimationFrame(() => animateTStep4(ms, { ...state }));
   } else {
     console.log("done T");
   }
   drawLineXForwards({
     initialCoords: [tStartPoint[0] + tXEndPoint2, tStartPoint[1]],
     yPositionOffset: tYEndPoint2 + tYEndPoint4,
-    length: xEnd4,
+    length: xEnd,
     colour: tColour,
   });
   drawLineYForwards({
     initialCoords: [tStartPoint[0], tStartPoint[1] + tYEndPoint],
-    endPoint: tXEndPoint - tXEndPoint2,
-    length: yEnd4,
+    endPoint: letterWidth - tXEndPoint2,
+    length: yEnd,
     colour: tColour,
   });
-  yEnd4++;
-  yEnd4++;
-  if (xEnd4 < tXEndPoint4) {
-    xEnd4++;
+  yEnd++;
+  yEnd++;
+  if (xEnd < tXEndPoint4) {
+    xEnd++;
   }
+  state = { ...state, xEnd, yEnd };
+}
+
+function drawLineYDiagonal(initialCoords, endCoords, variable, colour) {
+  ctx.beginPath();
+  ctx.moveTo(initialCoords[0] + variable, initialCoords[1]);
+  ctx.lineTo(endCoords[0], endCoords[1]);
+  ctx.strokeStyle = colour;
+  ctx.stroke();
+}
+
+function animateAFinal(ms, aFinalState) {
+  let state = { ...aFinalState };
+  let { aXEndFinal, aXEndPointFinal, aStartCoordsFinal } = state;
+  if (aXEndFinal < aXEndPointFinal) {
+    requestAnimationFrame((ts) => animateAFinal(ts, { ...state }));
+  } else {
+    console.log("done AFinal");
+    // initiate final flourish
+    // lightShow();
+  }
+
+  ctx.putImageData(finalImage, 0, 0);
+  finalImage = ctx.getImageData(0, 0, width, height);
+  ctx.beginPath();
+  ctx.moveTo(aStartCoordsFinal[0] + aXEndFinal / 1.35, aStartCoordsFinal[1]);
+  ctx.lineTo(aStartCoordsFinal[0] + aXEndFinal, aStartCoordsFinal[1]);
+  ctx.strokeStyle = "yellow";
+  ctx.stroke();
+  aXEndFinal = aXEndFinal + 7;
+  state = { ...state, aXEndFinal };
+}
+
+function animateA(ms, aState) {
+  let state = { ...aState };
+  let {
+    aStartCoords,
+    aEndCoords,
+    aEndCoords2,
+    aXEnd,
+    aColour,
+    aXEnd2,
+    s,
+  } = state;
+  if (aXEnd < letterWidth / 2) {
+    requestAnimationFrame((ts) => animateA(ts, state));
+  } else {
+    drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
+  }
+  aColour = `rgb(${150}, ${10 + ((aXEnd * 1) % 255)}, ${
+    10 + ((aXEnd * 2) % 255)
+  })`;
+  ctx.putImageData(imageData, aStartCoords[0] - 5, aStartCoords[1] - 5);
+  drawGrid();
+  drawLineYDiagonal(aStartCoords, aEndCoords, aXEnd, aColour);
+  drawLineYDiagonal(aStartCoords2, aEndCoords2, -aXEnd2, aColour);
+  aXEnd = aXEnd + 0.35;
+  aXEnd2 = aXEnd2 + 0.35;
+  state = { ...state, aXEnd, aXEnd2 };
+}
+
+function animateD(ms, dState) {
+  let state = { ...dState };
+  let { dXEnd, dYEnd, dStartPoint, dYEndPoint } = state;
+  if (dYEnd < dYEndPoint) {
+    window.requestAnimationFrame((ts) => animateD(ts, state));
+  } else {
+    window.requestAnimationFrame((ts) => animateDStep2(ts, state));
+  }
+  drawLineYForwards({
+    initialCoords: dStartPoint,
+    endPoint: 0,
+    length: dYEnd,
+    colour: `rgb(${150}, ${10 + ((dYEnd * 1) % 255)}, ${
+      10 + ((dYEnd * 2) % 255)
+    })`,
+  });
+  ctx.beginPath();
+  ctx.ellipse(
+    dStartPoint[0],
+    dStartPoint[1] + letterHeight / 2,
+    letterWidth,
+    letterHeight / 2,
+    0,
+    -Math.PI / 2,
+    dXEnd
+  );
+  ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd * 1) % 255)}, ${
+    10 + ((dYEnd * 2) % 255)
+  })`;
+  ctx.stroke();
+  dYEnd++;
+  if (dXEnd < Math.PI / 2) {
+    dXEnd = dXEnd + 0.05;
+  }
+  state = { ...state, dYEnd, dXEnd };
+}
+
+function animateDStep2(ms, dState) {
+  let state = { ...dState };
+  let { dStartPoint, dXEnd2, dYEnd2, dXEndPoint, dYEndPoint2 } = state;
+
+  if (dYEnd2 < dYEndPoint2) {
+    window.requestAnimationFrame((ts) => animateDStep2(ts, state));
+  } else {
+    console.log("done D");
+
+    finalImage = ctx.getImageData(0, 0, width, height);
+
+    requestAnimationFrame((ts) => animateAFinal(ts, aFinalInitialState));
+  }
+  drawLineYForwards({
+    initialCoords: [dStartPoint[0], dStartPoint[1] + width * 0.04],
+    endPoint: dXEndPoint,
+    length: dYEnd2,
+    colour: `rgb(${150}, ${10 + ((dYEnd2 * 2) % 255)}, ${
+      50 + ((dYEnd2 * 3) % 255)
+    })`,
+  });
+  ctx.beginPath();
+  ctx.ellipse(
+    dStartPoint[0] + dXEndPoint,
+    dStartPoint[1] + dYEndPoint2 / 2 + width * 0.04,
+    dYEndPoint2 / 2 + width * 0.065,
+    dYEndPoint2 / 2,
+    0,
+    -Math.PI / 2,
+    dXEnd2
+  );
+  ctx.strokeStyle = `rgb(${150}, ${10 + ((dYEnd2 * 1) % 255)}, ${
+    10 + ((dYEnd2 * 3) % 255)
+  })`;
+  ctx.stroke();
+  dYEnd2++;
+  if (dXEnd2 < Math.PI / 2) {
+    dXEnd2 = dXEnd2 + 0.06;
+  }
+
+  state = { ...state, dYEnd2, dXEnd2 };
 }
 
 function drawLineXForwards({ initialCoords, yPositionOffset, length, colour }) {
@@ -339,28 +461,24 @@ function drawLineYForwards({ initialCoords, endPoint, length, colour }) {
   ctx.stroke();
 }
 
-function fillBackground() {
-  ctx.fillStyle = "rgb(200, 100, 100)";
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, width, height);
-}
+// function fillBackground() {
+//   ctx.fillStyle = "rgb(200, 100, 100)";
+//   ctx.fillStyle = "black";
+//   ctx.fillRect(0, 0, width, height);
+// }
 
-function flashBackground() {
+function flashBorder() {
   iFlash += 3;
-  jFlash += 1;
-  if (iFlash <= 3000) {
-    window.requestAnimationFrame(flashBackground);
+  if (iFlash <= 1000) {
+    window.requestAnimationFrame(flashBorder);
   } else {
-    console.log("done");
+    console.log("finished flashing");
   }
-  const red = iFlash < 255 ? iFlash % 255 : jFlash;
-  const green = iFlash > 255 && iFlash < 510 ? iFlash % 255 : jFlash;
-  const blue = iFlash > 510 && iFlash < 765 ? iFlash % 255 : jFlash;
 
   const hue = iFlash % 360;
 
   const borderColour = `hsl(${hue}, ${`100%`}, ${`50%`})`;
-  canvas.style.border = `5px solid ${borderColour}`;
+  canvas.style.border = `2px solid ${borderColour}`;
 }
 
 // supposed to be like fox searchlight pictures torch thing
